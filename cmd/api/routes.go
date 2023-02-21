ddc1ce44a6c6c8437f0b90c991de8b4145f191b6
Daiyr Bokan <frogice10@gmail.com>
2023-02-21 15:01:07 +0600

    Bokanchik

---
+++ cmd/api/routes.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+	// initialize a new httprouter router instance
+	router := httprouter.New()
+
+	// here we convert default notFound response of router to our custom method
+	// in order to send JSON response
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	// also convert methodNotAllowed response to our custom method
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
+	// now register relevant methods and handlers for our endpoints
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/books", app.createBookHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/books/:id", app.showBookHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/books/:id", app.updateBookHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/books/:id", app.deleteBookHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/books", app.listBooksHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
+	// return router instance
+	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
+}
---
+++ internal/data/models.go
@@ -0,0 +1,31 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+// Define a custom ErrRecordNotFound error. We'll return this from our Get() method when
+// looking up a movie that doesn't exist in our database.
+var (
+	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
+)
+
+// Create a Models struct which wraps the MovieModel. We'll add other models to this,
+// like a UserModel and PermissionModel, as our build progresses.
+type Models struct {
+	Users  UserModel
+	Tokens TokenModel
+	Books  BookModel
+}
+
+// For ease of use, we also add a New() method which returns a Models struct containing
+// the initialized MovieModel.
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Users:  UserModel{DB: db}, // initialize a new UserModel instance
+		Tokens: TokenModel{DB: db},
+		Books:  BookModel{DB: db},
+	}
+}
